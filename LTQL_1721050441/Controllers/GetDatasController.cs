a34fdc49e9eadc85c133d4e03738657fbd87f007
dainhan88 <82584738+dainhan88@users.noreply.github.com>
2021-12-21 13:29:36 +0700

    Create_GetData_PVN441

---
+++ LTQL_1721050441/Controllers/GetDatasController.cs
@@ -0,0 +1,23 @@
+﻿using LTQL_1721050441.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTQL_1721050441.Controllers
+{
+    public class GetDatasController : Controller
+    {
+        // GET: GetDatas
+        PVN1721050441DbContext db = new PVN1721050441DbContext();
+        // GET: View2chucnang
+        public ActionResult Index()
+        {
+            var List = from st in db.NhaCungCap441s
+                       join s in db.PVNSanPham441s on st.MaNhaCungCap equals s.MaNhaCungCap
+                       select new GetData { NhaCungCap441s = st, pVNSanPham441s = s };
+            return View(List);
+        }
+    }
+}
---
+++ LTQL_1721050441/Models/GetData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050441.Models
+{
+    public class GetData
+    {
+        public NhaCungCap441 NhaCungCap441s { get; set; }
+        public PVNSanPham441 pVNSanPham441s { get; set; }
+    }
+}
