16518b0986612ede643c0bd2b299b74f1f5e4692
dainhan88 <82584738+dainhan88@users.noreply.github.com>
2021-12-21 13:08:49 +0700

    Create_Table_NCC_PVN441_2

---
+++ LTQL_1721050441/Models/PVNSanPham441.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050441.Models
+{
+    public class PVNSanPham441
+    {
+        [Key]
+        [Display(Name = "Mã Sản Phẩm")]
+        [StringLength(20)]
+        public string MaSanPham { get; set; }
+
+        [Display(Name = "Tên Sản Phẩm")]
+        [StringLength(50)]
+        public string TenSanPham { get; set; }
+
+        [Display(Name ="Mã Nhà Cung Cấp")]
+        public int MaNhaCungCap { get; set; }
+        [ForeignKey("MaNhaCungCap")]
+        public virtual NhaCungCap441 NhaCungCap441s { get; set; }
+    }
+}
